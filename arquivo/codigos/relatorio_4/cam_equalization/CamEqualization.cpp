5a2d43adabe98c4666f5bb3e7393a5889af7bd59
rdays <34921280+rdays@users.noreply.github.com>
2024-11-21 20:52:53 -0300

    Atualizando codigo

--- arquivo/codigos/relatorio_4/cam_equalization/CamEqualization.cpp
+++ arquivo/codigos/relatorio_4/cam_equalization/CamEqualization.cpp
@@ -10,6 +10,21 @@ int main( int argc, char** argv )
 {	
     VideoCapture cap(argc > 1 ? atoi(argv[1]) : 0);
     Mat src, dst;
+ 
+    int histSize = 256;
+ 
+    float range[] = { 0, 256 }; //the upper boundary is exclusive
+    const float* histRange[] = { range };
+ 
+    bool uniform = true, accumulate = false;
+ 
+    Mat original_hist, equalized_hist; 
+    
+    int hist_w = 512, hist_h = 400;
+    int bin_w = cvRound( (double) hist_w/histSize );
+ 
+    Mat histOriginal( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+    Mat histEqualized( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
     
     while (true)
     {
@@ -29,7 +44,27 @@ int main( int argc, char** argv )
         if (key == 'q' || key == 27)
         {
 			imwrite("original_grayscale.jpg", src);
-			imwrite("grayscale_equalized.jpg", dst);
+			imwrite("grayscale_equalized.jpg", dst);			
+			
+			calcHist( &src, 1, 0, Mat(), original_hist, 1, &histSize, histRange, uniform, accumulate );
+			calcHist( &dst, 1, 0, Mat(), equalized_hist, 1, &histSize, histRange, uniform, accumulate );
+ 
+			normalize(original_hist, original_hist, 0, histOriginal.rows, NORM_MINMAX, -1, Mat() );
+			normalize(equalized_hist, equalized_hist, 0, histOriginal.rows, NORM_MINMAX, -1, Mat() );
+			
+			for( int i = 1; i < histSize; i++ )
+			{
+				line( histOriginal, Point( bin_w*(i-1), hist_h - cvRound(original_hist.at<float>(i-1)) ),
+					  Point( bin_w*(i), hist_h - cvRound(original_hist.at<float>(i)) ),
+					  Scalar( 255, 0, 0), 2, 8, 0  );
+				line( histEqualized, Point( bin_w*(i-1), hist_h - cvRound(equalized_hist.at<float>(i-1)) ),
+					  Point( bin_w*(i), hist_h - cvRound(equalized_hist.at<float>(i)) ),
+					  Scalar( 0, 255, 0), 2, 8, 0  );
+			}
+			
+			imwrite("original_histogram.jpg", histOriginal);
+			imwrite("equalized_histogram.jpg", histEqualized);
+			
             break;
         }
 	}
